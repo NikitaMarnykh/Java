664b1d885868c8ac207c969572c2b008383a0445
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-10-13 12:28:41 +0300

    Update Container.java
    Added constructors without arguments for the "Node" and "Container" classes

--- laba_1/src/Container.java
+++ laba_1/src/Container.java
@@ -1,8 +1,15 @@
 public class Container<template> {
+    private Node<template> node;
+
     static class Node<template> {
         template item;
         Node<template> next;
         Node<template> previous;
+
+        Node() {}
     }
 
+    Container() {
+        node = new Node<>();
+    }
 }

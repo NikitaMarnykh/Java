bf9b4113aab8557612fdeafee8a966dd4b1c3546
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-13 23:50:43 +0300

    Update Execute.java
    В ходе проектирования класса решено было использовать паттерн проектирования singleton. Создана переменная instance для хранения единственного экземпляра класса Execute и хэш-таблица PRIORITY для хранения приоритета операций. Добавлен конструктор по умолчанию и метод get_instance для получения экземпляра класса.

--- laba_2/src/main/java/org/example/Execute.java
+++ laba_2/src/main/java/org/example/Execute.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Stack;
+
+
 public class Execute
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static Execute instance;
+    private static final HashMap<Character, Integer> PRIORITY;
+
+    static {
+        PRIORITY = new HashMap<>();
+        PRIORITY.put('(', 0);
+        PRIORITY.put('+', 1);
+        PRIORITY.put('-', 1);
+        PRIORITY.put('*', 2);
+        PRIORITY.put('/', 2);
+        PRIORITY.put('^', 3);
+        PRIORITY.put(')', 0);
+    }
+
+    private Execute() {
+    }
+
+    public static Execute get_instance() {
+        if (instance == null)
+            instance = new Execute();
+        return instance;
     }
 }

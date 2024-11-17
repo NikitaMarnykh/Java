dd609cfd9abc3bcfc3e845125335af999fa58756
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-17 12:56:59 +0300

    Update Execute.java
    Добавлен javadoc

--- laba_2/src/main/java/org/example/Execute.java
+++ laba_2/src/main/java/org/example/Execute.java
@@ -4,9 +4,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Stack;
 
+/**
+ * Основной класс для выполнения арифметических операций.
+ * Этот класс реализует алгоритм преобразования инфиксной нотации в постфиксную
+ * (обратную польскую нотацию) и выполнения арифметических операций на
+ * полученной постфиксной записи выражения.
+ */
+public class Execute {
 
-public class Execute
-{
     private static Execute instance;
     private static final HashMap<Character, Integer> PRIORITY;
 
@@ -21,20 +26,38 @@ public class Execute
         PRIORITY.put(')', 0);
     }
 
+    /**
+     * Приватный конструктор для реализации паттерна Singleton.
+     */
     private Execute() {
     }
 
+    /**
+     * Получает экземпляр класса Execute (Singleton).
+     *
+     * @return экземпляр Execute
+     */
     public static Execute get_instance() {
         if (instance == null)
             instance = new Execute();
         return instance;
     }
 
+    /**
+     * Преобразует строку инфиксного выражения в постфиксное.
+     * Этот метод принимает строку, представляющую арифметическое выражение в
+     * инфиксной нотации и возвращает строку с этим же выражением в постфиксной
+     * нотации.
+     *
+     * @param infix_str Строка с арифметическим выражением в инфиксной нотации.
+     * @return Строка с арифметическим выражением в постфиксной нотации.
+     */
     String infix_to_postfix(String infix_str) {
         char[] chars = infix_str.toCharArray();
         String postfix = "";
         Stack<Character> stack = new Stack<>();
         int index = 0;
+
         while (index < chars.length) {
             char chr = chars[index];
 
@@ -64,18 +87,28 @@ public class Execute
             index++;
         }
 
-        while (!(stack.isEmpty())) {
+        while (!stack.isEmpty()) {
             postfix += stack.pop();
         }
 
         return postfix;
     }
 
+    /**
+     * Выполняет арифметическое выражение в постфиксной нотации.
+     * Этот метод принимает строку, представляющую арифметическое выражение
+     * в постфиксной нотации и вычисляет его значение.
+     *
+     * @param postfix_str Строка с арифметическим выражением в постфиксной нотации.
+     * @return Результат выполнения выражения в виде числа с плавающей точкой.
+     * @throws ArithmeticException В случае деления на ноль или неправильного выражения.
+     */
     double execute(String postfix_str) {
         Stack<Double> stack = new Stack<>();
         char[] chars = postfix_str.toCharArray();
         Scanner scanner = new Scanner(System.in);
         int index = 0;
+
         while (index < chars.length) {
             char chr = chars[index];
             is_valid_char(chr);
@@ -97,6 +130,7 @@ public class Execute
             } else if (PRIORITY.containsKey(chr)) {
                 double operand2 = stack.pop();
                 double operand1 = stack.pop();
+
                 switch (chr) {
                     case '+':
                         stack.push(operand1 + operand2);
@@ -135,25 +169,59 @@ public class Execute
         return answer;
     }
 
+    /**
+     * Проверяет, является ли символ оператором.
+     *
+     * @param chr Символ для проверки.
+     * @return true, если символ - оператор, иначе false.
+     */
     boolean is_operator(char chr) {
         return PRIORITY.containsKey(chr);
     }
 
+    /**
+     * Проверяет, является ли символ цифрой.
+     *
+     * @param chr Символ для проверки.
+     * @return true, если символ - цифра, иначе false.
+     */
     boolean is_digit(char chr) {
         return Character.isDigit(chr);
     }
 
+    /**
+     * Проверяет, является ли символ буквой.
+     *
+     * @param chr Символ для проверки.
+     * @return true, если символ - буква, иначе false.
+     */
     boolean is_letter(char chr) {
         return Character.isLetter(chr);
     }
 
-     void is_valid_char(char chr) {
+    /**
+     * Проверяет, является ли символ допустимым для арифметического выражения.
+     *
+     * @param chr Символ для проверки.
+     * @throws RuntimeException Если символ недопустим.
+     */
+    void is_valid_char(char chr) {
         if (!Character.isDigit(chr) && !Character.isLetter(chr) && !is_operator(chr)
                 && chr != '_' && chr != '.' && chr != ' ' && chr != '(' && chr != ')') {
             throw new RuntimeException("Недопустимый символ: " + chr);
         }
     }
 
+    /**
+     * Проверяет правильность расположения десятичной точки.
+     *
+     * Должен проверяться индекс десятичной точки, чтобы убедиться, что
+     * она расположена правильно, то есть между двумя цифрами.
+     *
+     * @param index Индекс, на который указывает точка.
+     * @param chars Символы выражения.
+     * @throws NumberFormatException Если формат десятичной точки некорректен.
+     */
     void check_for_valid_decimal_point(int index, char[] chars) throws NumberFormatException {
         if (chars[index] != '.') {
             throw new NumberFormatException("Индекс должен указывать на точку.");
@@ -163,12 +231,28 @@ public class Execute
         }
     }
 
+    /**
+     * Проверяет, является ли символ допустимым для чисел.
+     *
+     * @param chr Символ для проверки.
+     * @throws RuntimeException Если символ недопустим.
+     */
     void is_valid_number_symbol(char chr) {
         if (!Character.isDigit(chr) && chr != '.') {
             throw new RuntimeException("Некорректный символ для числа: " + chr);
         }
     }
 
+    /**
+     * Находит границу левого края числа или переменной.
+     *
+     * Этот метод помогает определить, как много символов следует считать
+     * частью числа или названия переменной, начиная с начального индекса.
+     *
+     * @param start_index Начальный индекс для поиска.
+     * @param chars Символы выражения.
+     * @return Количество символов, представляющих число или переменную.
+     */
     int left_border(int start_index, char[] chars) {
         int ind = start_index;
         boolean is_digit_first = Character.isDigit(chars[start_index]);
@@ -197,5 +281,4 @@ public class Execute
 
         return ind - start_index - 1;
     }
-
 }

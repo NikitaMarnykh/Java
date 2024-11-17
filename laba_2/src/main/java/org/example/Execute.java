075f8777a1134887954289b6d357e481b99c31a0
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-17 12:05:55 +0300

    Update Execute.java
    Добавлен метод execute() выполняющий пример из строки

--- laba_2/src/main/java/org/example/Execute.java
+++ laba_2/src/main/java/org/example/Execute.java
@@ -71,6 +71,70 @@ public class Execute
         return postfix;
     }
 
+    double execute(String postfix_str) {
+        Stack<Double> stack = new Stack<>();
+        char[] chars = postfix_str.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        int index = 0;
+        while (index < chars.length) {
+            char chr = chars[index];
+            is_valid_char(chr);
+
+            if (chars[index] == '.') {
+                check_for_valid_decimal_point(index, chars);
+            } else if (Character.isDigit(chr) || Character.isLetter(chr) || chr == '_') {
+                int left_bord = left_border(index, chars);
+                String var_or_num = "";
+                for (int ind = index; ind <= index + left_bord; ind++) {
+                    var_or_num += chars[ind];
+                }
+                if (Character.isLetter(chars[index])) {
+                    System.out.print("Введите значение переменной " + var_or_num + ":\n");
+                    var_or_num = scanner.nextLine();
+                }
+                index += left_bord;
+                stack.push(Double.parseDouble(var_or_num));
+            } else if (PRIORITY.containsKey(chr)) {
+                double operand2 = stack.pop();
+                double operand1 = stack.pop();
+                switch (chr) {
+                    case '+':
+                        stack.push(operand1 + operand2);
+                        break;
+                    case '-':
+                        stack.push(operand1 - operand2);
+                        break;
+                    case '*':
+                        stack.push(operand1 * operand2);
+                        break;
+                    case '/':
+                        if (operand2 == 0) {
+                            throw new ArithmeticException("Деление на ноль");
+                        }
+                        stack.push(operand1 / operand2);
+                        break;
+                    case '^':
+                        stack.push(Math.pow(operand1, operand2));
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Неверный оператор: " + chr);
+                }
+            }
+
+            index++;
+        }
+
+        double answer = stack.pop();
+
+        if (!stack.isEmpty()) {
+            throw new ArithmeticException("В выражении пропущен оператор");
+        }
+
+        scanner.close();
+
+        return answer;
+    }
+
     boolean is_operator(char chr) {
         return PRIORITY.containsKey(chr);
     }

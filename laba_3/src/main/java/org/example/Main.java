79ce565da7f482c12cb7463aadaf31cce931c7ae
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-27 18:09:59 +0300

    Update Main.java

--- laba_3/src/main/java/org/example/Main.java
+++ laba_3/src/main/java/org/example/Main.java
@@ -1,13 +1,11 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class Main
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Method       Size    ArrayList_Time    LinkedList_Time");
     }
 }

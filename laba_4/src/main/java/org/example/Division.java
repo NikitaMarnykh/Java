f6041ac3cd18fd78accc5802dd6db49f6898e5b6
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-28 16:39:54 +0300

    Update Division.java
    Создан класс данных Division для хранения и работы с информацией о подразделении

--- laba_4/src/main/java/org/example/Division.java
+++ laba_4/src/main/java/org/example/Division.java
@@ -1,5 +1,22 @@
 package org.example;
 
 public class Division {
+  // Поля (атрибуты) класса Division
+  private final int id;              // Уникальный идентификатор подразделения
+  private final String name;         // Название подразделения
 
+  // Конструктор класса Division
+  Division(int id, String name) {
+    this.id = id;                    // Инициализация идентификатора
+    this.name = name;                // Инициализация названия
+  }
+
+  // Геттер для получения идентификатора
+  int getId()  { return id;                       // Возвращаем идентификатор }
+
+  // Геттер для получения названия
+  String getName() { return name;                     // Возвращаем название }
+
+  // Метод для получения строкового представления объекта Division
+    String dataToString() { return "ID: " + id + ", Name: " + name; // Формируем строку с информацией о подразделении }
 }

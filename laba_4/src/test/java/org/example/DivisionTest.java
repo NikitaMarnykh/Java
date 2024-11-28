05c7f6d391da85de5ae97de2909f45b94e23a3a9
Никита Марных <158211423+NikitaMarnykh@users.noreply.github.com>
2024-11-28 18:22:25 +0300

    Update DivisionTest.java
    Создан класс тестирвоания класса данных Division. Добавлен метод тестирования конструктора

--- laba_4/src/test/java/org/example/DivisionTest.java
+++ laba_4/src/test/java/org/example/DivisionTest.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DivisionTest {
+
+  @Test
+  public void testDivisionConstructor() {
+    // Тестируем конструктор класса Division
+    int expectedId = 1;
+    String expectedName = "Test Division";
+
+    // Создаем новый объект Division
+    Division division = new Division(expectedId, expectedName);
+
+    // Проверяем, что идентификатор и название инициализировались правильно
+    Assertions.assertEquals(expectedId, division.getId(), "ID должен быть равен" + expectedId);
+    Assertions.assertEquals(expectedName, division.getName(), "Название должео быть" + expectedName);
+  }
+
 }
